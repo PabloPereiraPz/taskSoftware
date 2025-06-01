fab0275982f9a34b712aa39d03623316ba58965f
PabloPcCap <pablo.a.cruz@capgemini.com>
2025-06-01 18:45:21 -0300

    master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component'; // importando o componente para aparecer na tela.
-import { UserComponent } from './user/user.component';
+import { UserComponent } from './user/user.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, UserComponent], // centralizar os components importando aqui dentro.
+  imports: [RouterOutlet, HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  standalone: true,
+  standalone: true
 })
-export class App {
+export class AppComponent {
   protected title = 'angular-complete-2025';
 }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header', // O @Component é um decorator, aqui o selector é ele selecionando este componente para mostrar o que está em HEADER.HTML na tela.
+  styleUrl: './header.component.css',
+  templateUrl: './header.component.html', // aqui é o arquivo HTML que será mostrado na tela quando chamar o APP-HEADER.
+  standalone: true,
+})
+export class HeaderComponent {}
